b99804a81982e091347f253334839f9f14e8877e
bryn <thenazboz@gmail.com>
2020-05-01 15:03:40 +0100

    added more info

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -2,34 +2,45 @@ import React, { Component } from 'react';
 
 const API = "https://api.coindesk.com/v1/bpi/currentprice/GBP.json"
 
+  
 
+   
 
   class Dashboard extends Component{
-
-    constructor( ){ 
-    super()
-      this.state={
-        coin:null
-      }
-    }
+    
+      state={
+        coin:"",
+        updateTime:""
+      } 
+    
 
   componentDidMount(){
       fetch(API)
       .then(res => res.json())
-      .then(data => this.setState={ coin : data.bpi.GBP.rate }  
-      )
-     
+      .then(data => this.setState( { coin : data.bpi.GBP.rate, updateTime : data.time.updateduk } ))
     }
 
-
+    
   render(){
   
   return (
     <div>
-    Price of BitCoin today is {this.state.coin}
+      
+      <h1>
+      Welcome back { }
+      </h1> 
+      
+      <p>
+      ₿itcoin £{this.state.coin.slice(0,-5)} - Last updated on {this.state.updateTime}
+      </p>
+
     </div>
   );
+
   
+  
+
+    
   
   }
 }
