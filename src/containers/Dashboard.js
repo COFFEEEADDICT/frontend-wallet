c8906f43fb0254aaa272c6d4b2af2afa0f82e554
github username <github email address>
2020-05-01 10:14:23 +0100

    fix

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,36 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-const API = "https://api.coindesk.com/v1/bpi/currentprice/GBP.json"
+const API = "https://api.coindesk.com/v1/bpi/currentprice/GBP.json";
 
+class Dashboard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      coin: null,
+    };
+  }
 
+  componentDidMount() {
+    fetch(API)
+      .then((res) => res.json())
+      .then((data) => this.setState({ coin: data.bpi.GBP.rate }));
+  }
 
-  class Dashboard extends Component{
-
-    constructor( ){ 
-    super()
-      this.state={
-        coin:null
-      }
-    }
-
-  componentDidMount(){
-      fetch(API)
-      .then(res => res.json())
-      .then(data => this.setState={ coin : data.bpi.GBP.rate }  
-      )
-     
-    }
-
-
-  render(){
-  
-  return (
-    <div>
-    Price of BitCoin today is {this.state.coin}
-    </div>
-  );
-  
-  
+  render() {
+    return <div>Price of BitCoin today is {this.state.coin}</div>;
   }
 }
 
